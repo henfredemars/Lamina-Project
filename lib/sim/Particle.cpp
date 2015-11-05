9aa666d39738406f36a0812dca999b1367a9fba6
henfredemars <henfredemars@users.noreply.github.com>
2015-11-05 18:28:48 -0500

    Implemented more of the build system, headers, and file format access plus implementation

---
+++ lib/sim/Particle.cpp
@@ -0,0 +1,45 @@
+
+#include "include/Particle.h"
+
+Particle::Particle(double x,double y,double z) {
+	this->x = x;
+	this->y = y;
+	this->z = z;
+}
+
+double Particle::getX() const {
+	return x;
+}
+
+double Particle::getY() const {
+	return y;
+}
+
+double Particle::getZ() const {
+	return z;
+}
+
+void Particle::setX(double x) {
+	this->x = x;
+}
+
+void Particle::setY(double y) {
+	this->y = y;
+}
+
+void Particle::setZ(double z) {
+	this->z = z;
+}
+
+double Particle::getRadius() const {
+	return 1;
+}
+
+double Particle::distanceTo(const Particle& p) const {
+	double xd = p.x-x;
+	double yd = p.y-y;
+	double zd = p.z-z;
+	return sqrt(xd*xd + yd*yd + zd*zd);
+}
+
+
--- lib/sim/include/Database.h
+++ lib/sim/include/Database.h
@@ -1,7 +1,12 @@
 //Header file for using system.db (project data file)
 
+#ifndef __DATABASE_H
+#define __DATABASE_H
+
 #include <vector>
 #include <string>
+#include <cstring>
+#include <iterator>
 #include "../../sqlite/sqlite3.h"
 #include "LaminaParticle.h"
 #include "SourceParticle.h"
@@ -11,14 +16,19 @@ class Database {
 public:
 	Database(std::string path);
 	~Database();
-	int getNumberOfGenerations() const;
-	int getTotalParticles() const;
-	int countParticlesInGeneration() const;
-	std::vector<LaminaParticle> getLaminaParticlesForGeneration(int generationNumber) const;
-	std::vector<LaminaParticle> getAllLaminaParticles() const;
-	std::vector<SourceParticle> getSourceParticles() const;
+	int getMaxGenerationNumber();
+	int getTotalParticlesInDB();
+	int countParticlesInGeneration(const int& generationNumber);
+	std::vector<LaminaParticle> getLaminaParticlesForGeneration(const int& generationNumber);
+	std::vector<LaminaParticle> getAllLaminaParticles();
+	std::vector<SourceParticle> getSourceParticles();
+	void insertSourceParticles(const std::vector<SourceParticle>& particles);
+	void insertLaminaParticles(const std::vector<LaminaParticle>& particles,const int& generationNumber);
 private:
-	sqlite3 db;
+	sqlite3* db;
+	void begin_transaction();
+	void end_transaction();
+	//int sqlite3_callback_read(void* unused_db,int result_cols,char** column_text,char** column_name);
 };
 
-
+#endif /* __DATABASE_H */
