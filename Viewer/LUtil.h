ae40ccdf5b1b25515f10dfdb1edf6521147c1b2d
henfredemars <marsrover7@fastmail.com>
2015-11-22 20:40:04 -0500

    Fixed accidental changes to my build system made by group partner

--- Viewer/LUtil.h
+++ Viewer/LUtil.h
@@ -4,9 +4,13 @@
 #define LUTIL_H
 
 #include "LOpenGL.h"
+#include "particles.h"
 #include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <string>
 
-
+using namespace std;
 
 //Screen constants
 const int SCREEN_WIDTH = 640;
@@ -15,7 +19,7 @@ const int SCREEN_FPS = 60;
 
 
 
-bool initGL();
+bool initGL(vector <Particles> particles_vec_in , vector <string> statistics_vec_in);
 
 void reshape(GLsizei size_x, GLsizei size_y);
 
@@ -24,6 +28,8 @@ void render();
 void handleKeys( unsigned char key, int x, int y );
 
 void handleMouse(int button, int state, int x, int y);
+
+void printText(float x, float y, string input_string);
 
 void resetCamera();
 
--- Viewer/main.cpp
+++ Viewer/main.cpp
@@ -9,6 +9,10 @@ https://en.wikibooks.org/wiki/OpenGL_Programming/
 */
 //*******************************************************
 #include "LUtil.h"
+#include <vector>
+#include <string>
+
+using namespace std;
 
 void runMainLoop( int val );
 
@@ -39,8 +43,33 @@ int main( int argc, char* args[] ) {
     //Set main loop
     glutTimerFunc( 1000 / SCREEN_FPS, runMainLoop, 0 );
     
+    //Setting the Lamina Particles vectors
+    //This is temporary
+    vector <Particles> particles_vector;
+    vector <string> statistics_vector;
+    Particles p1(0.0f, 5.0f, -15.0f, 1.0f);
+    Particles p2(6.0f, 4.0f, -10.0f, 1.0f);
+    Particles p3(-5.0f, 5.0f, -5.0f, 3.0f);
+    Particles p4(-3.0f, 5.0f, -15.0f, 1.0f);
+
+    particles_vector.push_back(p1);
+    particles_vector.push_back(p2);
+    particles_vector.push_back(p3);
+    particles_vector.push_back(p4);
+    
+    string s1 = "These are statistics for particle 1";
+    string s2 = "Particle 2's statistics";
+    string s3 = "Third particles's statistics are here";
+    string s4 = "Statistics for the 4th particle";
+    
+    statistics_vector.push_back(s1);
+    statistics_vector.push_back(s2);
+    statistics_vector.push_back(s3);
+    statistics_vector.push_back(s4);
+    
+    
     //Do initialization
-    if( !initGL() ) {
+    if( !initGL( particles_vector , statistics_vector) ) {
         printf( "Unable to run graphics application!\n" );
         return 1;
     }
